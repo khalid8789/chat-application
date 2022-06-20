e844366675fb2f5cfae16c5aa7d67fbd2d1efe8c
khalid8789 <olalerekhalid@gmail.com>
2022-06-20 07:29:27 -0700

    react chat application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from 'react'
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM  from 'react-dom'
+import App from './App'
+import {BrowserRouter} from "react-router-dom"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <StrictMode>
+<BrowserRouter>
+<App/>
+
+</BrowserRouter>
+</StrictMode>, 
+document.getElementById('root'));
