8bdf790019ba2d3ec6cb14b4f5f6fa5b5de2dd26
khalid8789 <olalerekhalid@gmail.com>
2022-06-20 08:20:46 -0700

    made changes to server.js file

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var app = express();
 
 app.use(express.static(__dirname + 'public'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+  if (process.env.NODE_ENV) {
+    app.use(express.static(path.resolve(process.cwd(), 'client/build')))
     app.get('*', (req, res) => {
-      res.sendFile(path.join('build', 'index.html'));
-    });
+      res.sendFile(path.resolve(process.cwd(), 'client/build/index.html'))
+    })
   }
 
 app.listen(process.env.PORT || 8080);
